ea2c5cbe85a148f71224f634b18de3b1960909b8
Patrick Gotthard <patrick@patrick-gotthard.de>
2014-04-05 15:44:37 +0200

    Added connection test query for Hikari Connection Pool

--- src/main/java/de/patrickgotthard/newsreadr/server/config/PersistenceConfig.java
+++ src/main/java/de/patrickgotthard/newsreadr/server/config/PersistenceConfig.java
@@ -42,6 +42,8 @@ public class PersistenceConfig {
         config.addDataSourceProperty("url", url);
         config.addDataSourceProperty("user", username);
         config.addDataSourceProperty("password", password);
+        config.setConnectionTestQuery("SELECT 1");
+
         return new HikariDataSource(config);
 
     }
