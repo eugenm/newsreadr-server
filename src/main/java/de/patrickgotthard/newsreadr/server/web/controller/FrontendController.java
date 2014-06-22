60ca608d6aaacdf640b06e42faeba54ccc33d7d1
Patrick Gotthard <patrick@patrick-gotthard.de>
2014-06-22 10:57:31 +0200

    Fixed import method (did not respond before)

--- src/main/java/de/patrickgotthard/newsreadr/server/web/controller/FrontendController.java
+++ src/main/java/de/patrickgotthard/newsreadr/server/web/controller/FrontendController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import de.patrickgotthard.newsreadr.server.service.SubscriptionService;
@@ -46,6 +47,7 @@ public class FrontendController {
     }
 
     @RequestMapping("/import")
+    @ResponseBody
     public Response importSubscriptions(final MultipartFile opmlFile) {
         return subscriptionService.importSubscriptions(opmlFile);
     }
