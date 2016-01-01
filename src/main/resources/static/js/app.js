c99b0f1e2d0cc249e60e44238b25cc3ee7c35f88
Patrick Gotthard <patrick@patrick-gotthard.de>
2016-01-01 23:45:58 +0100

    Renamed profile page and controller

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,55 +1,52 @@
 ﻿var newsreadrControllers = angular.module('newsreadrControllers', []);
 
-var newsreadrApp = angular.module('newsreadrApp', [
-    'ngRoute',
-    'ngSanitize',
-    'ngUpload',
-    'angular-loading-bar',
-	'infinite-scroll',
-    'newsreadrControllers'
-]);
+var newsreadrApp = angular.module('newsreadrApp', ['ngRoute', 'ngSanitize', 'ngUpload', 'angular-loading-bar', 'infinite-scroll', 'newsreadrControllers']);
 
 newsreadrApp.config(function($httpProvider) {
-	$httpProvider.interceptors.push(['$q', function($q) {
-		return {
-			responseError: function(rejection) {
-				if (rejection.status == 401) {
-					window.location.replace("login?timeout");
-				}
-				return $q.reject(rejection);
-			}
-		};
-	}]);
+
+    $httpProvider.interceptors.push(['$q', function($q) {
+        return {
+            responseError : function(rejection) {
+                if (rejection.status == 401) {
+                    window.location.replace("login?timeout");
+                }
+                return $q.reject(rejection);
+            }
+        };
+    }]);
+
 });
 
 newsreadrApp.config(['$sceProvider', function($sceProvider) {
-	$sceProvider.enabled(false);
+    $sceProvider.enabled(false);
 }]);
 
-newsreadrApp.config(['$routeProvider',
-                     
-    function($routeProvider) {
-	
-		$routeProvider.
-			when('/entries', {
-				controller: 'EntriesController',
-				templateUrl: 'partial/entries'
-			}).
-			when('/subscriptions', {
-				controller: 'SubscriptionsController',
-				templateUrl: 'partial/subscriptions'
-			}).
-			when('/users', {
-				controller: 'UsersController',
-				templateUrl: 'partial/users'
-			}).
-			when('/profile', {
-				controller: 'AccountController',
-				templateUrl: 'partial/account'
-			}).
-			otherwise({
-				redirectTo: '/entries'
-			});
-	}
+newsreadrApp.config(['$routeProvider', function($routeProvider) {
+
+    // entries
+    $routeProvider.when('/entries', {
+        controller : 'EntriesController',
+        templateUrl : 'partial/entries'
+
+    // subscriptions
+    }).when('/subscriptions', {
+        controller : 'SubscriptionsController',
+        templateUrl : 'partial/subscriptions'
+
+    // users
+    }).when('/users', {
+        controller : 'UsersController',
+        templateUrl : 'partial/users'
+
+    // profile
+    }).when('/profile', {
+        controller : 'ProfileController',
+        templateUrl : 'partial/profile'
+
+    // fallback
+    }).otherwise({
+        redirectTo : '/entries'
+    });
+}
 
 ]);
