4e674331c353c89d577500eacc493e3dc2c4b82f
Patrick Gotthard <patrick.gotthard@md.de>
2016-08-31 16:20:57 +0200

    Removes @EnableTransactionManagement annotation

--- src/main/java/de/patrickgotthard/newsreadr/server/Application.java
+++ src/main/java/de/patrickgotthard/newsreadr/server/Application.java
@@ -2,13 +2,10 @@ package de.patrickgotthard.newsreadr.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AdviceMode;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 public class Application {
 
     public static void main(final String[] args) {
