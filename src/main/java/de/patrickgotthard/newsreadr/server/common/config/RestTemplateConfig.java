1c5a5a91c0045a4a95961181f51f9907b2106f25
Patrick Gotthard <patrick@patrick-gotthard.de>
2016-09-05 20:56:28 +0200

    Solves a socket timeout issue

--- src/main/java/de/patrickgotthard/newsreadr/server/common/config/RestTemplateConfig.java
+++ src/main/java/de/patrickgotthard/newsreadr/server/common/config/RestTemplateConfig.java
@@ -1,10 +1,12 @@
 package de.patrickgotthard.newsreadr.server.common.config;
 
-import java.util.Collections;
-
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -15,15 +17,14 @@ class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(final RestTemplateBuilder builder) {
 
-        builder.setConnectTimeout(TEN_SECONDS);
-        builder.setReadTimeout(TEN_SECONDS);
+        final RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(TEN_SECONDS).setConnectionRequestTimeout(TEN_SECONDS)
+                .setSocketTimeout(TEN_SECONDS).build();
 
-        final RestTemplate template = builder.build();
-        template.setInterceptors(Collections.singletonList((request, body, execution) -> {
-            request.getHeaders().add("User-Agent", "newsreadr");
-            return execution.execute(request, body);
-        }));
-        return template;
+        final CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+
+        final HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(client);
+
+        return builder.requestFactory(factory).build();
 
     }
 
